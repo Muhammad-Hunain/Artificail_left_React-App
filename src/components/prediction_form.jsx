0563011f9e85c1adf59c68cbfb2ea422760dfe21
Muhammad-hunain <hunain29.mem@gmail.com>
2024-10-20 15:56:09 +0500

    done

--- src/components/prediction_form.jsx
+++ src/components/prediction_form.jsx
@@ -103,7 +103,7 @@ const PredictionForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://artificail-lifter-flask-api.vercel.app//predict', {
         method: 'POST',
         credentials: 'include',
         headers: {
